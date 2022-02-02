c7cfc6ff4e774ee8e8efdfd589c08041f55bca49
Ryan Kan <kanryan@gmail.com>
2022-02-01 18:07:20 -0800

    sd test

---
+++ sd.java
@@ -0,0 +1,5 @@
+public class sd {
+    public static int mutitply(int a, int b) {
+        return a*b;
+    }
+}
---
+++ sdTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class sdTest {
+    @Test 
+    public void testMultiply() {
+        assertEquals(8, sd.mutitply(1, 5));
+    }
+}
